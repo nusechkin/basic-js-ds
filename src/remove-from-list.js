b6756933169d66aaf573abd9700cc93337fe6cd7
nusechkin <komarova.ao@gmail.com>
2023-04-17 14:38:01 +0300

    Feat: remove from list

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function removeKFromList(l, k) {
+  let current = l;
+  let result = l;
+  let prev = null;
+
+  while (current !== null) {
+    if (current.value === k) {
+      if (prev === null) {
+        current = current.next;
+        result = current;
+      }
+      else {
+        prev.next = current.next;
+
+      }
+    }
+    else {
+      prev = current;
+    }
+    current = current.next;
+  }
+  return result;
 }
 
 module.exports = {
